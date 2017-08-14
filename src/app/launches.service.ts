f041a0d319b2387ae701f181ac486c149fc72572
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-14 15:04:49 -0600

    getting next 10 launches

--- src/app/launches.service.ts
+++ src/app/launches.service.ts
@@ -7,5 +7,12 @@ export class LaunchesService {
 
   async getNextTen() {
     console.log('asdf')
+    try {
+      const nextTen = await fetch('https://launchlibrary.net/1.1/launch/next/10')
+      const nextTenJSON = await nextTen.json()
+      console.log(nextTenJSON)
+    } catch (error) {
+      console.log('Error getting launch data: ' + error)
+    }
   }
 }
