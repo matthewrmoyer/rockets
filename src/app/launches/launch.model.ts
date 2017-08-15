de97fae9040472bf3c899a7abe8a73461c628e31
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-14 20:26:23 -0600

    fix duplicate bug

--- src/app/launches/launch.model.ts
+++ src/app/launches/launch.model.ts
@@ -1,21 +1,26 @@
 export class Launch {
     static instances = 0;
-    static launchList= []
+    static launchList = []
     constructor(
         public id: number,
         public name: string,
         public windowStart: string,
         public windowEnd: string,
         public streamUrls: string[],
-        public infoUrls: string [],
+        public infoUrls: string[],
         public status,
         public holdReason,
         public failReason,
         public location: object,
         public missions: object[],
         public rocket: object,
-        ) {
+    ) {
+        const duplicate = Launch.launchList.find(dup => {
+            return dup.id === this.id;
+        })
+        if (!duplicate) {
             Launch.instances++
             Launch.launchList.push(this)
         }
+    }
 }
