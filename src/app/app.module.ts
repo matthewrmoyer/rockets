8649f60b0f69c2008d1a7f2d8474b5a2e5bcb930
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-14 21:12:57 -0600

    materialize setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
-import appRoutes from './app.routes'
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MdButtonModule, MdButtonToggle, MdCheckboxModule, MdCardModule, MdIconModule} from '@angular/material';
+import 'hammerjs';
+
+import appRoutes from './app.routes'
 import {LaunchesService} from './launches.service';
 
 @NgModule({
@@ -13,6 +16,10 @@ import {LaunchesService} from './launches.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    // refactor all material design stuff into own module... https://material.angular.io/guide/getting-started
+    MdButtonModule,
+    MdCheckboxModule,
     appRoutes
   ],
   providers: [LaunchesService],
--- src/app/launches/launches.module.ts
+++ src/app/launches/launches.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LaunchesComponent } from './launches.component';
 import launchesRoutes from './launches.routes';
 
+// refactor material stuff into its own module
+import {MdButtonModule, MdButtonToggleModule, MdCheckboxModule, MdCardModule, MdIconModule} from '@angular/material';
+
+
 @NgModule({
   imports: [
     CommonModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCheckboxModule,
+    MdCardModule,
+    MdIconModule,
     launchesRoutes
   ],
   declarations: [LaunchesComponent]
