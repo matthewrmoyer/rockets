351fa614fd6ae0b246d46fe97b53e6fb661ebc8d
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-20 17:19:32 -0600

    add links to sidebar

--- src/app/side-navbar/side-navbar.component.ts
+++ src/app/side-navbar/side-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Launch} from '../launches/launch.model'
 
 @Component({
   selector: 'app-side-navbar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavbarComponent implements OnInit {
 
+  launchList;
   constructor() { }
 
   ngOnInit() {
+    console.log(Launch.launchList)
+    this.launchList = Launch.launchList
   }
 
 }
