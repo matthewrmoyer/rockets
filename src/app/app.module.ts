6e365ad29cf913cc80d322c884d07b879c9adc6f
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-24 17:12:44 -0600

    move reactiveformsmodule to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppComponent } from './app.component';
 import { CustomAngularMaterialModule } from './custom-angular-material/custom-angular-material.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import 'hammerjs';
+
 import { CalendarModule } from 'angular-calendar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import appRoutes from './app.routes'
@@ -27,6 +29,7 @@ import { SearchComponent } from './search/search.component';
     CustomAngularMaterialModule,
     BrowserAnimationsModule,
     appRoutes,
+    ReactiveFormsModule
   ],
   providers: [LaunchesService],
   bootstrap: [AppComponent]
--- src/app/custom-angular-material/custom-angular-material.module.ts
+++ src/app/custom-angular-material/custom-angular-material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // tslint:disable-next-line:max-line-length
 import {MdCardModule, MdToolbarModule, MdSidenavModule, MdButtonModule, MdButtonToggleModule,  MdMenuModule, MdCheckboxModule, MdIconModule} from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+// import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
 
 @NgModule({
@@ -16,7 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
     MdMenuModule,
     MdCheckboxModule,
     MdIconModule,
-    ReactiveFormsModule
+    // ReactiveFormsModule
   ],
   exports: [
     MdCardModule,
@@ -27,7 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
     MdMenuModule,
     MdCheckboxModule,
     MdIconModule,
-    ReactiveFormsModule
+    // ReactiveFormsModule
   ],
   declarations: []
 })
