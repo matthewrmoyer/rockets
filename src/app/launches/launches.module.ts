8bf905146a7edc599a515b2fade07067781808fe
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-29 16:58:08 -0600

    not importing angular material module anywhere except shared module

--- src/app/launches/launches.module.ts
+++ src/app/launches/launches.module.ts
@@ -6,13 +6,11 @@ import { LaunchesComponent } from './launches.component';
 import { SideNavbarComponent } from '../side-navbar/side-navbar.component'
 
 
-// import { CustomAngularMaterialModule } from '../custom-angular-material/custom-angular-material.module'
 import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   imports: [
     CommonModule,
-    // CustomAngularMaterialModule,
     SharedModule,
     launchesRoutes
     ],
--- src/app/rocket-calendar/rocket-calendar.module.ts
+++ src/app/rocket-calendar/rocket-calendar.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RocketCalendarComponent } from './rocket-calendar.component'
-import { CustomAngularMaterialModule } from '../custom-angular-material/custom-angular-material.module'
+import { SharedModule } from '../shared/shared.module'
 import calendarRoutes from './rocket-calendar.routes'
 
 @NgModule({
   imports: [
     CommonModule,
-    CustomAngularMaterialModule,
+    SharedModule,
     calendarRoutes
   ],
   declarations: [RocketCalendarComponent]
