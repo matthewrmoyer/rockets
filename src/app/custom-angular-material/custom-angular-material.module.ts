4e3d3c3c1d2d2bb322f3321291b0136e02380c5a
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-29 16:27:10 -0600

    erase imports from material module

--- src/app/custom-angular-material/custom-angular-material.module.ts
+++ src/app/custom-angular-material/custom-angular-material.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // tslint:disable-next-line:max-line-length
-import {MdCardModule, MdToolbarModule, MdSidenavModule, MdButtonModule, MdButtonToggleModule,  MdMenuModule, MdCheckboxModule, MdIconModule} from '@angular/material';
+import {MdCardModule, MdToolbarModule, MdSidenavModule, MdButtonModule, MdButtonToggleModule,  MdMenuModule, MdCheckboxModule, MdIconModule, MdAutocompleteModule} from '@angular/material';
 // import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
-import {MdAutocompleteModule} from '@angular/material';
 
 
 
 @NgModule({
   imports: [
     CommonModule,
-    MdCardModule,
-    MdToolbarModule,
-    MdSidenavModule,
-    MdButtonModule,
-    MdButtonToggleModule,
-    MdMenuModule,
-    MdCheckboxModule,
-    MdIconModule,
-    // ReactiveFormsModule
-    MdAutocompleteModule
+    // MdCardModule,
+    // MdToolbarModule,
+    // MdSidenavModule,
+    // MdButtonModule,
+    // MdButtonToggleModule,
+    // MdMenuModule,
+    // MdCheckboxModule,
+    // MdIconModule,
+    // // ReactiveFormsModule
+    // MdAutocompleteModule
   ],
   exports: [
     MdCardModule,
