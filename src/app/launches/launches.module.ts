be4a44ed10f0e3f3db217ad9690b12f92ef9fa1a
Matthew Moyer <matthewrmoyer@gmail.com>
2017-08-29 16:55:48 -0600

    not importing angular material module into launches module

--- src/app/launches/launches.module.ts
+++ src/app/launches/launches.module.ts
@@ -6,12 +6,14 @@ import { LaunchesComponent } from './launches.component';
 import { SideNavbarComponent } from '../side-navbar/side-navbar.component'
 
 
-import { CustomAngularMaterialModule } from '../custom-angular-material/custom-angular-material.module'
+// import { CustomAngularMaterialModule } from '../custom-angular-material/custom-angular-material.module'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   imports: [
     CommonModule,
-    CustomAngularMaterialModule,
+    // CustomAngularMaterialModule,
+    SharedModule,
     launchesRoutes
     ],
   declarations: [LaunchesComponent, SideNavbarComponent]
